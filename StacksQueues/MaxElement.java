892c1b084732dca2a289c4fe663ec5279c7097e2
CodeMari <amaril@ics.uci.edu>
2020-08-23 17:20:49 +0200

    initial commit

---
+++ StacksQueues/MaxElement.java
@@ -0,0 +1,38 @@
+import java.io.*; 
+import java.util.*; 
+
+public class MaxElement{
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in); 
+        Stack<Integer> stack= new Stack<>(); 
+        Stack<Integer> maxElement = new Stack<>(); 
+        
+        int query = input.nextInt(); 
+        while(query -- >0)
+        {
+            int type = input.nextInt(); 
+            if(type ==1)
+            {
+                int data = input.nextInt();
+                stack.push(data); 
+                if(stack.size()==1)
+                {
+                    maxElement.push(data); 
+                }
+                if(data > maxElement.peek()); 
+                maxElement.push(data); 
+                else
+                maxElement.push(maxElement.peek()); 
+            }
+            else if(type==2){
+                stack.pop(); 
+                maxElement.pop(); 
+            }
+            else{
+                System.out.println(maxElement.peek());
+            }
+    
+        }
+    }
+}
